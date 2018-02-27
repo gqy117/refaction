ca34861946123acdbf57224a869b2c336f46f3ad
Leo Li <gqy117@gmail.com>
2018-02-27 19:28:36 +1300

    [refactor] Moved the logic from ProductsController.cs to ProductsService.cs

--- refactor-me/Controllers/ProductsController.cs
+++ refactor-me/Controllers/ProductsController.cs
@@ -2,66 +2,55 @@
 using System.Net;
 using System.Web.Http;
 using refactor_me.Models;
+using refactor_me.Service;
 
 namespace refactor_me.Controllers
 {
     [RoutePrefix(Consts.Products.ProductsRoute)]
     public class ProductsController : ApiController
     {
+        public ProductsService ProductsService { get; set; } = new ProductsService();
+
         [Route]
         [HttpGet]
         public Products GetAll()
         {
-            return new Products();
+            return this.ProductsService.GetAll();
         }
 
         [Route]
         [HttpGet]
         public Products SearchByName(string name)
         {
-            return new Products(name);
+            return this.ProductsService.SearchByName(name);
         }
 
         [Route(Consts.Products.Id)]
         [HttpGet]
         public Product GetProduct(Guid id)
         {
-            var product = new Product(id);
-            if (product.IsNew)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-
-            return product;
+            return this.ProductsService.GetProduct(id);
         }
 
         [Route]
         [HttpPost]
         public void Create(Product product)
         {
-            product.Save();
+            this.ProductsService.Create(product);
         }
 
         [Route(Consts.Products.Id)]
         [HttpPut]
         public void Update(Guid id, Product product)
         {
-            var orig = new Product(id)
-            {
-                Name = product.Name,
-                Description = product.Description,
-                Price = product.Price,
-                DeliveryPrice = product.DeliveryPrice
-            };
-
-            if (!orig.IsNew)
-                orig.Save();
+            this.ProductsService.Update(id, product);
         }
 
         [Route(Consts.Products.Id)]
         [HttpDelete]
         public void Delete(Guid id)
         {
-            var product = new Product(id);
-            product.Delete();
+            this.ProductsService.Delete(id);
         }
     }
 }
---
+++ refactor-me/Service/ProductsService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using refactor_me.Models;
+
+namespace refactor_me.Service
+{
+    public class ProductsService
+    {
+        public Products GetAll()
+        {
+            return new Products();
+        }
+
+        public Products SearchByName(string name)
+        {
+            return new Products(name);
+        }
+
+        public Product GetProduct(Guid id)
+        {
+            var product = new Product(id);
+            if (product.IsNew)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return product;
+        }
+
+        public void Create(Product product)
+        {
+            product.Save();
+        }
+
+        public void Update(Guid id, Product product)
+        {
+            var orig = new Product(id)
+            {
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                DeliveryPrice = product.DeliveryPrice
+            };
+
+            if (!orig.IsNew)
+                orig.Save();
+        }
+
+        public void Delete(Guid id)
+        {
+            var product = new Product(id);
+            product.Delete();
+        }
+    }
+}
