e0a875117f872bea9303c09d7b9fe411b36cd051
Leo Li <gqy117@gmail.com>
2018-02-27 19:50:45 +1300

    [refactor] Moved the logic from ProductOptionsController.cs to ProductOptionsService.cs.

--- refactor-me/Controllers/ProductOptionsController.cs
+++ refactor-me/Controllers/ProductOptionsController.cs
@@ -2,58 +2,47 @@
 using System.Net;
 using System.Web.Http;
 using refactor_me.Models;
+using refactor_me.Service;
 
 namespace refactor_me.Controllers
 {
     [RoutePrefix(Consts.ProductOptions.ProductOptionsRoute)]
     public class ProductOptionsController : ApiController
     {
+        public ProductOptionsService ProductOptionsService { get; set; } = new ProductOptionsService();
         [Route]
         [HttpGet]
         public ProductOptions GetOptions(Guid productId)
         {
-            return new ProductOptions(productId);
+            return this.ProductOptionsService.GetOptions(productId);
         }
 
         [Route(Consts.ProductOptions.Id)]
         [HttpGet]
         public ProductOption GetOption(Guid productId, Guid id)
         {
-            var option = new ProductOption(id);
-            if (option.IsNew)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-
-            return option;
+            return this.ProductOptionsService.GetOption(id);
         }
 
         [Route]
         [HttpPost]
         public void CreateOption(Guid productId, ProductOption option)
         {
-            option.ProductId = productId;
-            option.Save();
+            this.ProductOptionsService.CreateOption(productId, option);
         }
 
         [Route(Consts.ProductOptions.Id)]
         [HttpPut]
         public void UpdateOption(Guid id, ProductOption option)
         {
-            var orig = new ProductOption(id)
-            {
-                Name = option.Name,
-                Description = option.Description
-            };
-
-            if (!orig.IsNew)
-                orig.Save();
+            this.ProductOptionsService.UpdateOption(id, option);
         }
 
         [Route(Consts.ProductOptions.Id)]
         [HttpDelete]
         public void DeleteOption(Guid id)
         {
-            var opt = new ProductOption(id);
-            opt.Delete();
+            this.ProductOptionsService.DeleteOption(id);
         }
     }
 }
---
+++ refactor-me/Service/ProductOptionsService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using refactor_me.Models;
+
+namespace refactor_me.Service
+{
+    public class ProductOptionsService
+    {
+        public ProductOptions GetOptions(Guid productId)
+        {
+            return new ProductOptions(productId);
+        }
+
+        public ProductOption GetOption(Guid id)
+        {
+            var option = new ProductOption(id);
+            if (option.IsNew)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return option;
+        }
+
+        public void CreateOption(Guid productId, ProductOption option)
+        {
+            option.ProductId = productId;
+            option.Save();
+        }
+
+        public void UpdateOption(Guid id, ProductOption option)
+        {
+            var orig = new ProductOption(id)
+            {
+                Name = option.Name,
+                Description = option.Description
+            };
+
+            if (!orig.IsNew)
+                orig.Save();
+        }
+
+        public void DeleteOption(Guid id)
+        {
+            var opt = new ProductOption(id);
+            opt.Delete();
+        }
+    }
+}
