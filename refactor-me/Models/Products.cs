456ead8e3da05dcc0a33032f88fe9aeca78f1e0d
Leo Li <Leo.Li@myob.com>
2018-02-28 16:14:50 +1300

    Removed Products.

--- refactor-me/Models/Products.cs
+++ refactor-me/Models/Products.cs
@@ -7,36 +7,6 @@ using Newtonsoft.Json;
 
 namespace refactor_me.Models
 {
-    public class Products
-    {
-        public List<Product> Items { get; private set; }
-
-        public Products()
-        {
-            LoadProducts(null);
-        }
-
-        public Products(string name)
-        {
-            LoadProducts($"where lower(name) like '%{name.ToLower()}%'");
-        }
-
-        private void LoadProducts(string where)
-        {
-            Items = new List<Product>();
-            var conn = Helpers.NewConnection();
-            var cmd = new SqlCommand($"select id from product {where}", conn);
-            conn.Open();
-
-            var rdr = cmd.ExecuteReader();
-            while (rdr.Read())
-            {
-                var id = Guid.Parse(rdr["id"].ToString());
-                Items.Add(new Product(id));
-            }
-        }
-    }
-
     [Table("Product")]
     public class Product
     {
